80121ddb681b79f981139da1901f5ad3c6e0590e
Olexa-git <oplutaev@gmail.com>
2022-10-19 21:32:11 +0300

    catalogue pagination functionality

---
+++ script/arrow_pagination.js
@@ -0,0 +1,153 @@
+import { catalogue } from './catalogue.js';
+let product_catalogue_page = 1;
+let review_page = 1;
+const catalogue_left_active_arrow = document.getElementById('left_active_arrow');
+const catalogue_left_inactive_arrow = document.getElementById('left_inactive_arrow');
+const catalogue_right_active_arrow = document.getElementById('right_active_arrow');
+const catalogue_right_inactive_arrow = document.getElementById('right_inactive_arrow');
+const catalogue_active_elipses = document.querySelectorAll('#pagination_ellipse_active');
+const catalogue_inactive_elipses = document.querySelectorAll('#pagination_ellipse_inactive');
+let review_pagination = document.getElementById('review_pagination');
+let review_right_arrow;
+let review_left_arrow;
+
+const catalogue_items_list = document.getElementById('catalogue_items_list');
+
+export function catalogue_pagination_right () {
+	catalogue_active_elipses[product_catalogue_page-1].classList.add('invisible');
+	catalogue_inactive_elipses[product_catalogue_page-1].classList.remove('invisible');
+	catalogue_active_elipses[product_catalogue_page].classList.remove('invisible');
+	catalogue_inactive_elipses[product_catalogue_page].classList.add('invisible');
+	product_catalogue_page++;
+	create_catalogue(product_catalogue_page);
+	if (product_catalogue_page == 2) {
+		catalogue_left_active_arrow.classList.remove('invisible');
+		catalogue_left_inactive_arrow.classList.add('invisible');	
+	} else if (product_catalogue_page == 5) {
+		catalogue_right_inactive_arrow.classList.remove('invisible');
+		catalogue_right_active_arrow.classList.add('invisible');
+	}
+}
+export function catalogue_pagination_left () {
+	catalogue_active_elipses[product_catalogue_page-1].classList.add('invisible');
+	catalogue_inactive_elipses[product_catalogue_page-1].classList.remove('invisible');
+	catalogue_active_elipses[product_catalogue_page-2].classList.remove('invisible');
+	catalogue_inactive_elipses[product_catalogue_page-2].classList.add('invisible');
+	product_catalogue_page--;
+	create_catalogue(product_catalogue_page);
+	if (product_catalogue_page == 4) {
+		catalogue_right_inactive_arrow.classList.add('invisible');
+		catalogue_right_active_arrow.classList.remove('invisible');
+	} else if (product_catalogue_page == 1) {
+		catalogue_left_active_arrow.classList.add('invisible');
+		catalogue_left_inactive_arrow.classList.remove('invisible');
+	}
+}
+
+export function create_review_pagination() {
+	const fragment = document.createDocumentFragment();
+	
+	for (let i = 0; i <= 6; i++) {
+		if (i == 0) {
+			const arrow = document.createElement('img');
+			if (review_page == 1) {
+				arrow.src = "images/Arrow_2.png";
+				arrow.classList.add('catalogue_pagination_arrow');
+				arrow.id = 'review_left_arrow';
+				fragment.appendChild(arrow);
+			} else {
+				arrow.src = "images/Arrow_1.png";
+				arrow.classList.add('catalogue_pagination_left_arrow_active');
+				arrow.id = 'review_left_arrow';
+				fragment.appendChild(arrow);
+				}
+		} else if (i == 6) {
+			const arrow = document.createElement('img');
+			if (review_page == 5) {
+				arrow.src = "images/Arrow_2.png";
+				arrow.classList.add('catalogue_pagination_right_arrow_inactive');
+				arrow.id = 'review_right_arrow';
+				fragment.appendChild(arrow);
+			} else {
+				arrow.src = "images/Arrow_1.png";
+				arrow.classList.add('catalogue_pagination_arrow');
+				arrow.id = 'review_right_arrow';
+				fragment.appendChild(arrow);
+				}
+		} else {
+			const elipse = document.createElement('img');
+			if (i == review_page) {
+				elipse.src = "images/Ellipse_1.png";
+				elipse.classList.add('catalogue_pagination_ellipse');
+				fragment.appendChild(elipse);
+			} else {
+				elipse.src = "images/Ellipse_2.png";
+				elipse.classList.add('catalogue_pagination_ellipse');
+				fragment.appendChild(elipse);
+				}
+			}
+		}
+	return fragment
+	}
+export function reviwe_pagination_right() {
+	if (review_page < 5) {
+		review_page++;
+		review_pagination.innerHTML = '';
+		review_pagination.appendChild(create_review_pagination());
+		review_right_arrow = document.getElementById('review_right_arrow');
+		review_right_arrow.addEventListener('click', reviwe_pagination_right);
+		review_left_arrow = document.getElementById('review_left_arrow');
+		review_left_arrow.addEventListener('click', reviwe_pagination_left);
+		}
+	}
+export function reviwe_pagination_left() {
+	if (review_page > 1) {
+		review_page--;
+		review_pagination.innerHTML = '';
+		review_pagination.appendChild(create_review_pagination());
+		review_right_arrow = document.getElementById('review_right_arrow');
+		review_right_arrow.addEventListener('click', reviwe_pagination_right);
+		review_left_arrow = document.getElementById('review_left_arrow');
+		review_left_arrow.addEventListener('click', reviwe_pagination_left);
+		}
+	}
+
+function create_catalogue_item(cat_item) {
+	const template = document.createElement('template');
+	const item = `<div class="catalogue_item">
+				  <img src="${cat_item.image}">
+				  <p>${cat_item.title}<br>
+				  ${cat_item.description}</p>
+				  <p>${cat_item.price}</p>
+				  <button class="catalogue_in_basket">In basket</button>
+				  </div>`;
+	template.innerHTML = item;
+	return template.content;
+}
+
+function create_catalogue (page) {
+	catalogue_items_list.innerHTML = '';
+	if (page == 1) {
+		for (let i=0; i<6; i++) {
+			catalogue_items_list.appendChild(create_catalogue_item(catalogue[i]));
+		}
+	} else if (page == 2) {
+		for (let i=2; i<8; i++) {
+			catalogue_items_list.appendChild(create_catalogue_item(catalogue[i]));
+		}
+	} else if (page == 3) {
+		for (let i=4; i<10; i++) {
+			catalogue_items_list.appendChild(create_catalogue_item(catalogue[i]));
+		}
+	} else if (page == 4) {
+		for (let i=6; i<12; i++) {
+			catalogue_items_list.appendChild(create_catalogue_item(catalogue[i]));
+		}
+	} else if (page == 5) {
+		for (let i=8; i<14; i++) {
+			catalogue_items_list.appendChild(create_catalogue_item(catalogue[i]));
+		}
+	}
+}
+create_catalogue(product_catalogue_page);
+
---
+++ script/catalogue.js
@@ -0,0 +1,71 @@
+export const catalogue = [{
+	image: 'images/bouquet_cart1.png',
+	title: 'Sorbet:',
+	description: 'roses and lilies',
+	price: '$15.99'
+}, {
+	image: 'images/bouquet_cart2.png',
+	title: 'Gentle freshness:',
+	description: 'chrysanthemums and roses',
+	price: '$17.99'
+}, {
+	image: 'images/bouquet_cart3.png',
+	title: 'Yellow Song:',
+	description: 'sunflowers and solidago',
+	price: '$14.99'
+}, {
+	image: 'images/bouquet_cart4.png',
+	title: 'Peach nectar:',
+	description: 'chrysanthemums and roses',
+	price: '$15.99'
+}, {
+	image: 'images/bouquet_cart5.png',
+	title: 'Aurora:',
+	description: 'lilies and roses',
+	price: '$12.99'
+}, {
+	image: 'images/bouquet_cart6.png',
+	title: 'Love classic:',
+	description: 'red roses',
+	price: '$18.99'
+}, {
+	image: 'images/bouquet_cart7.jpg',
+	title: 'Fresh breath:',
+	description: 'eucalyptus populus',
+	price: '$11.99'
+}, {
+	image: 'images/bouquet_cart8.jpg',
+	title: 'Green peace:',
+	description: 'white freesias',
+	price: '$16.99'
+}, {
+	image: 'images/bouquet_cart9.jpg',
+	title: 'Notes of spring:',
+	description: 'eucalyptus cinerea',
+	price: '$12.99'
+}, {
+	image: 'images/bouquet_cart10.jpg',
+	title: 'Tropical forest:',
+	description: 'monstera',
+	price: '$8.99'
+}, {
+	image: 'images/bouquet_cart11.jpg',
+	title: 'Youth:',
+	description: 'rose bombastic',
+	price: '$12.99'
+}, {
+	image: 'images/bouquet_cart12.jpg',
+	title: 'Lady:',
+	description: 'rose lady bombastic',
+	price: '$14.99'
+}, {
+	image: 'images/bouquet_cart13.jpg',
+	title: 'Palette:',
+	description: 'delphinium and peonies',
+	price: '$22.99'
+}, {
+	image: 'images/bouquet_cart14.jpg',
+	title: 'Rainbow:',
+	description: 'hyacinths and zamiokulkas',
+	price: '$17.99'
+}]
